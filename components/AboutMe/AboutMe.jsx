f2b05be685f46875e3665fb128de53a17ce1a261
Thiago Puntar <thiagopuntar@gmail.com>
2022-03-19 19:52:26 -0300

    fix: About me

---
+++ components/AboutMe/AboutMe.jsx
@@ -0,0 +1,51 @@
+import React from "react";
+import { MdArrowRightAlt } from "react-icons/md";
+import imgHero2 from "../../public/img/img-hero-2.jpg";
+import { linkedinLink, resume } from "../../constants/links";
+import ResponsiveImage from "../ResponsiveImage";
+import {
+  H1,
+  ContentWrapper,
+  Wrapper,
+  ButtonWrapper,
+  StyledButton,
+  Span,
+  LinkWrapper,
+} from "./styles";
+
+const AboutMe = () => {
+  return (
+    <Wrapper>
+      <ResponsiveImage src={imgHero2} alt="Thiago Castro" />
+      <ContentWrapper>
+        <H1>Um pouco sobre mim</H1>
+        <p>
+          No último ano decidi mudar de vida, me mudei para Portugal e migrei de
+          carreira para UX Design.
+        </p>
+        <br />
+        <p>
+          Tecnologia e inovação sempre foi uma paixão minha, adoro artes e criar
+          desenhos e imagens. Descobrir no UX uma nova paixão.
+        </p>
+        <LinkWrapper>
+          <Span>Saiba mais sobre mim</Span>
+          <MdArrowRightAlt size={20} />
+        </LinkWrapper>
+        <ButtonWrapper>
+          <StyledButton onClick={() => window.open(resume, "_blank")}>
+            Baixe o meu currículo
+          </StyledButton>
+          <StyledButton
+            variant="outlined"
+            onClick={() => window.open(linkedinLink, "_blank")}
+          >
+            Conecte-se no Linkedin
+          </StyledButton>
+        </ButtonWrapper>
+      </ContentWrapper>
+    </Wrapper>
+  );
+};
+
+export default AboutMe;
---
+++ components/AboutMe/styles.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+import styled from "styled-components";
+import Button from "../Button";
+
+export const H1 = styled.h1`
+  font-family: Poppins;
+  font-size: 18px;
+  margin-bottom: 16px;
+`;
+
+export const ContentWrapper = styled.div`
+  margin-top: 24px;
+
+  & > p {
+    font-size: 14px;
+  }
+`;
+
+export const Wrapper = styled.div`
+  margin-top: 40px;
+`;
+
+export const ButtonWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-end;
+  gap: 16px;
+  margin-top: 20px;
+`;
+
+export const StyledButton = styled(Button)`
+  width: 192px;
+`;
+
+export const Span = styled.span`
+  font-size: 14px;
+`;
+
+export const LinkWrapper = styled.a`
+  margin-top: 16px;
+  display: flex;
+  align-items: center;
+  gap: 10px;
+  & * {
+    color: ${(props) => props.theme.colors.primary.zero1};
+  }
+`;
