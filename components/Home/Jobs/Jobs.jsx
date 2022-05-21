06681ac5f97a9e622825091d0446f4d28d0f4a72
Thiago Puntar <thiagopuntar@gmail.com>
2022-05-21 13:00:09 -0300

    fix(home): add finish job

--- components/Home/Jobs/Jobs.jsx
+++ components/Home/Jobs/Jobs.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import JobCard from "./JobCard";
+import jobFinish from "../../../public/img/job-finish.png";
 import jobPortfolio from "../../../public/img/job-portfolio.png";
 import jobCrypto from "../../../public/img/job-crypto.png";
 import { H2, JobsWrapper, Wrapper } from "./styles";
@@ -10,11 +11,22 @@ const Jobs = () => {
       <H2 id="works">Trabalhos</H2>
       <JobsWrapper>
         <JobCard
+          imageSrc={jobFinish}
+          imageAlt="Finish"
+          header="Aplicativo Financeiro"
+          title="Neon Finish"
+          href="/jobs/finni"
+        >
+          Como os correntistas do Banco Neon controlam suas finanças e ao mesmo
+          tempo economizam para ter uma melhor qualidade de vida.
+        </JobCard>
+        <JobCard
           imageSrc={jobPortfolio}
           imageAlt="Portifólio"
           header="Criação do Portifólio"
           title="Portifólio"
           href="/jobs/portifolio"
+          align="right"
         >
           Como foi pensado e estruturado o desenvolvimento desse portifólio.
           Utilizando ferramentas e técnicas de UX.
@@ -26,7 +38,6 @@ const Jobs = () => {
           header="Aplicativo de NFT"
           title="Cyber Crypto"
           href="/jobs/cyberCrypto"
-          align="right"
         >
           Desenvolvimento de design para um aplicativo de NFT, com elementos de
           carteira e compra de NFT
