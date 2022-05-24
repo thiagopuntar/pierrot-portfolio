593dd6c2526b00201f9983e247e9fb2aac537f5c
Thiago Puntar <thiagopuntar@gmail.com>
2022-05-24 11:32:41 -0300

    fix(crypto): format links

--- components/Jobs/Crypto.jsx
+++ components/Jobs/Crypto.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ResponsiveImage from "../ResponsiveImage";
-import { Columns, H1, H2, H3, Paragraph, StyledImage } from "./styles";
+import { Columns, H1, H2, H3, Ul, Paragraph, StyledImage } from "./styles";
 import imgHero from "../../public/img/jobs/crypto/hero.png";
 import imgProject from "../../public/img/jobs/crypto/project.png";
 import imgWireframe from "../../public/img/jobs/crypto/wireframe.png";
@@ -36,42 +36,75 @@ const Crypto = () => {
           aplicados de UX para o bom uso do aplicativo.
         </Paragraph>
         <H3>Mas o que seria NFT?</H3>
+        <Paragraph>Segundo pesquisas descobri que NFT é:</Paragraph>
         <Paragraph>
-          Segundo pesquisas descobri que NFT é: A sigla para o termo non
-          fungible token, ou “token não fungível”. Eles são tokens, ou seja,
-          códigos numéricos com registro de transferência digital que garantem
-          autenticidade aos seus donos. Na prática, eles funcionam como itens
-          colecionáveis, que não podem ser reproduzidos, mas sim transferidos.
-          Diferente das criptomoedas, como o Bitcoin, e vários tokens
-          utilitários, os NFTs não são mutuamente intercambiáveis. O token não
-          fungível representa algo específico e individual, com cartão de
-          &#34;autenticidade&#34; e que não pode ser substituído. Por exemplo,
-          um bitcoin é fungível - troque um por outro bitcoin e você terá
-          exatamente a mesma coisa. Os NFTs são projetados para dar a você algo
-          que não pode ser copiado: propriedade da obra (embora o artista ainda
-          possa reter os direitos autorais e de reprodução da obra física). Essa
-          relação entre dados e valores – realizada pela NFT – é possível por
-          causa de uma tecnologia chamada Blockchain. Esta é uma base de dados
-          descentralizada que ficou famosa por servir de fundamento para as
-          criptomoedas, como a Bitcoin e também a Ethereum. É um sistema onde é
-          possível rastrear o recebimento e envio de determinados tipos de
-          informações. Todas as transações ficam registradas em um tipo de
-          documento oficial que pode ser acessado por qualquer usuário. A
-          maioria dos NFTs faz parte do Blockchain Ethereum. Os NFTs são
-          armazenados em carteiras digitais (embora seja importante notar que a
-          carteira precisa ser especificamente compatível com NFT).
+          A sigla para o termo non fungible token, ou “token não fungível”. Eles
+          são tokens, ou seja, códigos numéricos com registro de transferência
+          digital que garantem autenticidade aos seus donos. Na prática, eles
+          funcionam como itens colecionáveis, que não podem ser reproduzidos,
+          mas sim transferidos. Diferente das criptomoedas, como o Bitcoin, e
+          vários tokens utilitários, os NFTs não são mutuamente intercambiáveis.
+          O token não fungível representa algo específico e individual, com
+          cartão de &#34;autenticidade&#34; e que não pode ser substituído. Por
+          exemplo, um bitcoin é fungível - troque um por outro bitcoin e você
+          terá exatamente a mesma coisa.
         </Paragraph>
         <Paragraph>
-          {" "}
+          Os NFTs são projetados para dar a você algo que não pode ser copiado:
+          propriedade da obra (embora o artista ainda possa reter os direitos
+          autorais e de reprodução da obra física). Essa relação entre dados e
+          valores – realizada pela NFT – é possível por causa de uma tecnologia
+          chamada Blockchain. Esta é uma base de dados descentralizada que ficou
+          famosa por servir de fundamento para as criptomoedas, como a Bitcoin e
+          também a Ethereum. É um sistema onde é possível rastrear o recebimento
+          e envio de determinados tipos de informações. Todas as transações
+          ficam registradas em um tipo de documento oficial que pode ser
+          acessado por qualquer usuário. A maioria dos NFTs faz parte do
+          Blockchain Ethereum. Os NFTs são armazenados em carteiras digitais
+          (embora seja importante notar que a carteira precisa ser
+          especificamente compatível com NFT).
+        </Paragraph>
+        <Paragraph spaced>
           fonte:
-          https://mundoconectado.com.br/artigos/v/21538/nft-o-que-e-e-como-funciona-a-tecnologia-que-da-milhoes-de-dolares-por-arte-digital.
+          <a
+            target="_blank"
+            href="https://mundoconectado.com.br/artigos/v/21538/nft-o-que-e-e-como-funciona-a-tecnologia-que-da-milhoes-de-dolares-por-arte-digital."
+            rel="noreferrer"
+          >
+            https://mundoconectado.com.br/artigos/v/21538/nft-o-que-e-e-como-funciona-a-tecnologia-que-da-milhoes-de-dolares-por-arte-digital.
+          </a>
         </Paragraph>
-        <Paragraph>
-          Saiba mais em:
-          https://valor.globo.com/financas/noticia/2021/12/21/o-que-e-nft-como-funciona-essa-tecnologia-posso-criar-um-veja-perguntas-e-respostas.ghtml
-          https://forbes.com.br/forbes-tech/2022/02/afinal-o-que-e-nft-e-como-criar-e-vender/
-          https://www.techtudo.com.br/noticias/2021/03/o-que-e-nft-entenda-como-funciona-a-tecnologia-do-token.ghtml
-        </Paragraph>
+        <Paragraph spaced>Saiba mais em:</Paragraph>
+        <Ul>
+          <li>
+            <a
+              target="_blank"
+              href="https://valor.globo.com/financas/noticia/2021/12/21/o-que-e-nft-como-funciona-essa-tecnologia-posso-criar-um-veja-perguntas-e-respostas.ghtml"
+              rel="noreferrer"
+            >
+              O que é NFT e como funciona essa tecnologia?
+            </a>
+          </li>
+          <li>
+            <a
+              target="_blank"
+              href="https://forbes.com.br/forbes-tech/2022/02/afinal-o-que-e-nft-e-como-criar-e-vender/"
+              rel="noreferrer"
+            >
+              A final o que é NFT e como criar e vender?
+            </a>
+          </li>
+          <li>
+            <a
+              target="_blank"
+              href="https://www.techtudo.com.br/noticias/2021/03/o-que-e-nft-entenda-como-funciona-a-tecnologia-do-token.ghtml"
+              rel="noreferrer"
+            >
+              Entenda como funciona a tecnologia do token
+            </a>
+          </li>
+        </Ul>
+
         <H3>Objetivo do projeto</H3>
         <Paragraph>
           Tornar o meu portifólio simples e de fácil acesso para qualquer
