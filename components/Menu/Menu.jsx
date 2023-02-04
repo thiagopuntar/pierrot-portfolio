dde4edc8082d835fea76a314f398d8c6632d1a4c
Thiago Puntar <thiagopuntar@gmail.com>
2023-02-04 18:38:36 -0300

    fix(menu): improve styles & refactor code

--- components/Menu/Menu.jsx
+++ components/Menu/Menu.jsx
@@ -3,61 +3,55 @@ import Link from "next/link";
 import { useRouter } from "next/router";
 import {
   HamburguerButton,
-  StyledLink,
   MenuWrapper,
+  MenuItemWrapper,
+  SubmenuItemWrapper,
+  SubMenu,
   SubMenuWrapper,
 } from "./styles";
 
-const MenuLink = ({
-  href,
-  name,
-  variant,
-  children,
-  onMouseEnter,
-  onMouseLeave,
-}) => {
+const MenuItem = ({ href, name, children, variant }) => {
   const router = useRouter();
   const isActive = router.asPath === href;
 
+  const ComponentWrapper =
+    variant === "submenu" ? SubmenuItemWrapper : MenuItemWrapper;
+
   return (
-    <StyledLink
-      onMouseEnter={onMouseEnter}
-      onMouseLeave={onMouseLeave}
-      isActive={isActive}
-      variant={variant}
-    >
-      <Link href={href} passHref>
-        <a>
-          {name}
-          {children}
-        </a>
-      </Link>
-    </StyledLink>
+    <ComponentWrapper isActive={isActive}>
+      {children || (
+        <span>
+          <Link href={href} passHref>
+            <a>{name}</a>
+          </Link>
+        </span>
+      )}
+    </ComponentWrapper>
   );
 };
 
+const SubmenuItem = (props) => <MenuItem {...props} variant="submenu" />;
+
 const Menu = () => {
   const [isOpen, setOpen] = useState(false);
-  const [isExpanded, setExpand] = useState(false);
 
   return (
     <>
       <HamburguerButton onClick={() => setOpen(!isOpen)} />
       <MenuWrapper isOpen={isOpen} onClick={() => setOpen(false)}>
-        <MenuLink name="Home" href="/" />
-        <MenuLink
-          name="Trabalhos"
-          href=""
-          onMouseEnter={() => setExpand(true)}
-          onMouseLeave={() => setExpand(false)}
-        >
-          <SubMenuWrapper isExpanded={isExpanded}>
-            <MenuLink name="Finni$" href="/jobs/finni$" />
-            <MenuLink name="Cyber Crypto" href="/jobs/cyberCrypto" />
-            <MenuLink name="Portifólio" href="/jobs/portifolio" />
+        <MenuItem name="Home" href="/" />
+        <MenuItem>
+          <span>Trabalhos</span>
+          <SubMenuWrapper>
+            <SubMenu>
+              <SubmenuItem name="Neon Finni$" href="/jobs/finni$" />
+              <SubmenuItem name="Krypton" href="/jobs/cyberCrypto" />
+              <SubmenuItem name="Coletânea" href="/jobs/collection" />
+              <SubmenuItem name="Portifólio" href="/jobs/portifolio" />
+            </SubMenu>
           </SubMenuWrapper>
-        </MenuLink>
-        <MenuLink name="Sobre Mim" href="/about" />
+        </MenuItem>
+        <MenuItem name="Sobre Mim" href="/about" />
       </MenuWrapper>
     </>
   );
--- components/Menu/styles.js
+++ components/Menu/styles.js
@@ -13,7 +13,7 @@ export const HamburguerButton = styled(FcMenu)`
 
 export const MenuWrapper = styled.ul`
   ${({ isOpen }) => (isOpen ? "display: flex" : "display: none")};
-  background-color: ${({ theme }) => theme.colors.white.z2};
+  background-color: var(--white-2);
   padding: 0.75rem;
   border-radius: 16px;
   flex-direction: column;
@@ -41,8 +41,8 @@ export const MenuWrapper = styled.ul`
   ${breakAt("md")} {
     display: flex;
     flex-direction: row;
-    position: relative;
     justify-content: flex-end;
+    position: relative;
     top: 0;
     right: 0;
     gap: 15%;
@@ -53,65 +53,105 @@ export const MenuWrapper = styled.ul`
 `;
 
 const activeMobileStyle = css`
-  border-bottom: 2px solid ${({ theme }) => theme.colors.secondary.z4};
+  border-bottom: 2px solid var(--secondary-4);
   font-weight: bold;
 `;
 
-const activeDesktopStyle = css`
-  color: ${({ theme }) => theme.colors.secondary.z4};
-  /* font-size: 1.25rem; */
-`;
+const MenuItemBase = styled.li`
+  font-size: 0.9rem;
+  color: var(--primary-1);
+  cursor: pointer;
 
-const contactStyle = css`
-  border: 2px solid ${({ theme }) => theme.colors.primary.z1};
-  border-radius: 20px;
-  color: ${({ theme }) => theme.colors.primary.z1};
-  padding: 0.5em 1em;
-  margin-left: 1.5em;
-`;
-
-export const StyledLink = styled.li`
-  & a {
+  a {
+    display: inline-block;
+    color: inherit;
     text-decoration: none;
-    font-size: 0.9rem;
+  }
 
-    ${breakUntil("md")} {
+  ${breakUntil("md")} {
+    & > span {
       ${({ isActive }) => isActive && activeMobileStyle};
     }
+  }
 
-    ${breakAt("md")} {
-      font-size: 1.125rem;
-      font-weight: bold;
-
-      ${({ isActive }) => isActive && activeDesktopStyle};
-    }
-
-    &:hover {
-      ${activeDesktopStyle};
-    }
-
-    ${({ variant }) => variant === "contact" && contactStyle}
+  ${breakAt("md")} {
+    font-size: 1.125rem;
   }
 `;
 
-export const SubMenuWrapper = styled.ul`
-  padding: 0.75rem;
+export const MenuItemWrapper = styled(MenuItemBase)`
+  ${breakAt("md")} {
+    font-weight: bold;
+
+    ${({ isActive }) => isActive && `color: var(--secondary-5)`};
+
+    span {
+      display: inline-block;
+      transition: transform 0.1s ease-in-out;
+    }
+
+    :hover > span {
+      transform: scale(1.1);
+    }
+  }
+`;
+
+export const SubmenuItemWrapper = styled(MenuItemBase)`
+  ${breakAt("md")} {
+    font-weight: normal;
+    ${({ isActive }) => isActive && `font-weight: 700;`};
+
+    span {
+      position: relative;
+      overflow: hidden;
+      padding: 4px;
+    }
+
+    span::after {
+      content: "";
+      position: absolute;
+      left: 0;
+      bottom: 0px;
+      height: 2px;
+      width: 100%;
+      transform: translateX(-100%);
+      background-color: var(--secondary-1);
+      transition: transform 0.2s ease-in-out;
+    }
+
+    span:hover::after {
+      transform: translateX(0);
+    }
+  }
+`;
+
+export const SubMenuWrapper = styled.div`
+  ${breakAt("md")} {
+    transform: translateY(20%);
+    visibility: hidden;
+    opacity: 0;
+    position: absolute;
+    padding-top: 8px;
+    transition: visibility 0.2s ease-in-out, transform 0.2s ease-in-out,
+      opacity 0.2s ease-in-out;
+
+    ${MenuItemWrapper}:hover & {
+      visibility: visible;
+      opacity: 1;
+      transform: translateY(0%);
+    }
+  }
+`;
+
+export const SubMenu = styled.ul`
   display: flex;
   flex-direction: column;
+  padding: 0.75rem;
   gap: 0.6rem;
 
   ${breakAt("md")} {
-    display: none;
-    position: absolute;
-    background-color: ${({ theme }) => theme.colors.white.z2};
+    background-color: var(--white-2);
     border-radius: 16px;
     padding: 1em;
-
-    & a {
-      font-size: 1rem;
-      font-weight: normal;
-    }
-
-    ${({ isExpanded }) => isExpanded && "display: flex;"}
   }
 `;
