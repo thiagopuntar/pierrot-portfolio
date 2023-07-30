6f038174ccd18a9e730275d2da07cb4bb7d991cc
Thiago Puntar <thiagopuntar@mbp-de-thiago.lan>
2023-07-30 10:05:03 -0300

    refactor: responsive image component

--- components/ResponsiveImage.jsx
+++ components/ResponsiveImage.jsx
@@ -6,28 +6,8 @@ const StyledImage = styled(Image)`
   border-radius: 24px;
 `;
 
-const ResponsiveImage = ({
-  className,
-  src,
-  alt,
-  height,
-  width,
-  layout,
-  objectFit,
-  priority = false,
-}) => {
-  return (
-    <StyledImage
-      className={className}
-      src={src}
-      alt={alt}
-      height={height}
-      width={width}
-      layout={layout}
-      objectFit={objectFit}
-      priority={priority}
-    />
-  );
+const ResponsiveImage = ({ className, priority = false, ...props }) => {
+  return <StyledImage className={className} priority={priority} {...props} />;
 };
 
 export default ResponsiveImage;
